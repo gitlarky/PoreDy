a84fac2a0a97e008b1f641f8f4e75ebe14d95aa4
Zhenyu Xu <westlark@outlook.com>
2019-10-30 17:38:01 +0800

    hey

--- script/SmartNet.py
+++ script/SmartNet.py
@@ -561,7 +561,7 @@ def CreatePoreNetworkSamples(Nx=20, Ny=20, Folder=''):
 							SampleIndex+=1
 							SampleIndex+=1
 	for B in [1, 3, 5, 7, 9]:
-		for SC in []:
+		for SC in [[0, 1, 2, 3, 4], [4, 5, 6, 7, 8], [0, 2, 4, 6, 8], [1, 3, 5, 7, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8]]:
 			for RG in [1, 2, 3, 4]:
 							SampleIndex+=1
 							SampleIndex+=1
